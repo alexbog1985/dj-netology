38d5d77bef7f0187551b23942e55f64b367f60c5
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-07-25 00:06:02 +0300

    hw complete

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        total_forms = 0
+        unique_tags = set()
+        is_main_count = 0
+        for form in self.forms:
+            if not form.cleaned_data or form.cleaned_data['DELETE']:
+                continue
+            total_forms += 1
+            unique_tags.add(form.cleaned_data['tag'])
+            if form.cleaned_data.get('is_main'):
+                is_main_count += 1
+        if is_main_count > 1:
+            raise ValidationError("Основным может быть только один раздел")
+        if total_forms > len(unique_tags):
+            raise ValidationError("Выбраны повторяющие разделы")
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,17 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, related_name='tags', through='Scope', verbose_name='Статьи')
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
