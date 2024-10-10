8c256c18d13961abe7c12bcbffee2346007b9d37
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-10-10 19:36:05 +0300

    start

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,17 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+
+from students.models import Student, Course
+
+
+@pytest.mark.django_db
+def test_api():
+    client = APIClient()
+    Student.objects.create(name='test', birth_date='2000-01-01')
+    Course.objects.create(name='test')
+
+    response = client.get('/api/v1/courses/')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 1
